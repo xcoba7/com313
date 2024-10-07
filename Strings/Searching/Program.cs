4ac01430b44d80ab64366c659daaf60f191c77c3
xcoba7 <iwuamadikingsley5@gmail.com>
2024-10-07 09:16:55 +0100

    Searching

--- Strings/Searching/Program.cs
+++ Strings/Searching/Program.cs
@@ -4,14 +4,20 @@
 string teststr = "The quick brown Fox jumps over the lazy Dog";
 
 // TODO: Contains determines whether a string contains certain content
-
+Console.WriteLine($"{teststr.Contains("fox")}");
+Console.WriteLine($"{teststr.Contains("fox", StringComparison.CurrentCultureIgnoreCase)}");
 
 // TODO: StartsWith and EndsWith determine if a string starts 
 // or ends with a given test string
-
+Console.WriteLine($"{teststr.StartsWith("the")}");
+Console.WriteLine($"{teststr.StartsWith("the",StringComparison.CurrentCultureIgnoreCase)}");
+Console.WriteLine($"{teststr.EndsWith("dog")}");
+Console.WriteLine($"{teststr.EndsWith("dog",StringComparison.CurrentCultureIgnoreCase)}");
 
 // TODO: IndexOf, LastIndexOf finds the index of a substring
-
+Console.WriteLine($"{teststr.IndexOf("the")}");
+Console.WriteLine($"{teststr.IndexOf("the",StringComparison.CurrentCultureIgnoreCase)}");
+Console.WriteLine($"{teststr.LastIndexOf("the")}");
 
 // TODO: Determining empty, null, or whitespace
 string str1 = null;
